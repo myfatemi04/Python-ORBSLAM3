8e77cb2356985a37d48cbee93b3bd479b4406faf
Michael Fatemi <myfatemi04@gmail.com>
2024-11-15 10:35:56 -0500

    Remove unnecessary dependency (GLEW was only used for Pangolin)

--- build.sh
+++ build.sh
@@ -1,48 +1,9 @@
-if [ which ninja ]; then
+#!/bin/sh
+
+if [ $(which ninja) ]; then
 	NINJA_GENERATOR="-GNinja"
 	echo "Using Ninja build generator for faster builds."
 fi
 
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF $NINJA_GENERATOR
 cmake --build build
-
-# echo "Configuring and building Thirdparty/DBoW2 ..."
-
-# cd Thirdparty/DBoW2
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
-# cd ../../g2o
-
-# echo "Configuring and building Thirdparty/g2o ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
-# cd ../../Sophus
-
-# echo "Configuring and building Thirdparty/Sophus ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j
-
-# cd ../../../
-
-# echo "Uncompress vocabulary ..."
-
-# cd Vocabulary
-# tar -xf ORBvoc.txt.tar.gz
-# cd ..
-
-# echo "Configuring and building ORB_SLAM3 ..."
-
-# mkdir build
-# cd build
-# cmake .. -DCMAKE_BUILD_TYPE=Release
-# make -j4
